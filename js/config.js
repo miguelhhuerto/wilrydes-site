7dfe78ca0b65d21c7b6513137f5d5d39d3d8dcf6
miguelhhuerto <127226426+miguelhhuerto@users.noreply.github.com>
2025-02-10 20:52:26 +0800

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'ap-southeast-2_OWyh8JIzv', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '34p3mucpq0aorcvar73pdfvc78', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'ap-southeast-2' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
